11af7bba98c574152532a0ff784a7ce9329039d3
Laurens Rietveld <laurens.rietveld@gmail.com>
2014-05-19 09:49:11 +0200

    added ga

--- main.js
+++ main.js
@@ -218,3 +218,14 @@ var deleteEveryDivExcept = function(divId) {
 	targetDiv.parents().show();
 	targetDiv.show();
 };
+
+/**
+ * draw google analytics
+ */
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-51130014-1', 'lodlaundry.github.io');
+  ga('send', 'pageview');
