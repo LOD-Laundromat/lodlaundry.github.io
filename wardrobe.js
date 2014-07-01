9c2dd3657662f490ed18b3c0c507edbcb5a9a1b8
Wouter Beek <me@wouterbeek.com>
2014-07-01 12:52:58 +0200

    Working on the info box in the wardrobe.

--- wardrobe.js
+++ wardrobe.js
@@ -110,7 +110,7 @@ var drawTable = function() {
       "dom": "frtipS",
       "deferRender": true,
       "createdRow": function (row, data, index) {
-        var md5 = $(row).find("td:nth-child(0)").text();
+        var md5 = data.md5;
         var triples = parseInt($(row).find("id:nth-child(4)").text());
         var cleanLink;
         if (triples == 0) {
