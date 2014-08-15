1cf37d04817a3c0eb8170a70dd399501a32f978c
Wouter Beek <me@wouterbeek.com>
2014-08-15 17:54:18 +0200

    Check whether default graph setting is working.

--- main.js
+++ main.js
@@ -12,9 +12,7 @@ var sparql = {
 totalTripleCount :
 "PREFIX ll: <http://lodlaundromat.org/vocab#>\n\
 SELECT (SUM(?triples) AS ?totalTriples) {\n\
-//  GRAPH <http://lodlaundromat.org#11> {\n\
-    ?dataset ll:triples ?triples . \n\
-//  }\n\
+  ?dataset ll:triples ?triples . \n\
 }\n",
 serializationsPerDoc :
 "PREFIX ll: <http://lodlaundromat.org/vocab#>\n\
