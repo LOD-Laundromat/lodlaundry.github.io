b196defd59004da1f89d7a6633a56115e3eea7df
Laurens Rietveld <laurens.rietveld@gmail.com>
2015-02-18 09:47:42 +0100

    fixed issue with cached default graphs and the recently upped crawl version

--- sparql.js
+++ sparql.js
@@ -5,8 +5,7 @@ $( document ).ready(function() {
 	$("#actualEndpoint").text(sparql.url).attr("href", sparql.url);
 	
 	var namedGraphs = getUrlParams("named-graph-uri");
-	var defaultGraphs = getUrlParams("default-graph-uri");
-	if (defaultGraphs.length == 0) defaultGraphs = [sparql.graphs.main, sparql.graphs.seedlist, sparql.graphs.metrics, sparql.graphs.error, sparql.graphs.http];
+	var defaultGraphs = [sparql.graphs.main, sparql.graphs.seedlist, sparql.graphs.metrics, sparql.graphs.error, sparql.graphs.http];
   YASGUI.YASQE.defaults.sparql.namedGraphs = namedGraphs;
   YASGUI.YASQE.defaults.sparql.defaultGraphs = defaultGraphs;
   YASGUI.YASQE.defaults.sparql.endpoint = sparql.url;
@@ -18,7 +17,13 @@ $( document ).ready(function() {
 		"	{[] ?properties ?x}\n"+
 		"} LIMIT 10";
   yasgui = YASGUI(document.getElementById("sparql"));
-	
+  //overwrite settings from local storage w.r.t. default graphs (needed when changing crawl version)
+  for (var tabId in yasgui.tabManager.tabs) {
+      var tab = yasgui.tabManager.tabs[tabId];
+      if (tab.persistentOptions && tab.persistentOptions.yasqe && tab.persistentOptions.yasqe.sparql) {
+          tab.persistentOptions.yasqe.sparql.defaultGraphs = defaultGraphs;
+      }
+  }
 });
 var getUrlParams = function(key) {
 	var values = [];
